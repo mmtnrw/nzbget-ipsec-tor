ed78840c64bf8a7cb887e219f124aef565a4dccc
root <root@tx1310.lan.mmt.nrw>
2021-10-18 16:04:34 +0200

    Error in Update Script....

--- root/update.sh
+++ root/update.sh
@@ -3,4 +3,5 @@ NZBGET_BRANCH="testing-download"
 mkdir -p /app/nzbget && \
 curl -o /tmp/json -L http://nzbget.net/info/nzbget-version-linux.json && NZBGET_VERSION=$(grep "${NZBGET_BRANCH}" /tmp/json  | cut -d '"' -f 4) && \
 curl -o /tmp/nzbget.run -L "${NZBGET_VERSION}" && \
-sh /tmp/nzbget.run --destdir /app/nzbget && \
+sh /tmp/nzbget.run --destdir /app/nzbget
+
